7fb3ab089832b2307ca3f8a700e3c9bf2cb8b82b
Jeremy Inman <jeremy.inman13@gmail.com>
2023-06-22 14:06:53 -0500

    Update streamlit_app.py
    added some icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,14 @@ import streamlit
 streamlit.title("My Parents New Healthy Dinner")
 
 #header 1: breakfast
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
 
+#icons
+icons = ['🥣', '🥗', '🐔', '🥑', '🍞']
 #breakfast items
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free Range Egg")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free Range Egg")
+streamlit.text("🥑🍞 Avacado Toast")
+
 
