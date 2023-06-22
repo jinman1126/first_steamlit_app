fb83699b43bc96b06510b6979dea962ab48d5022
Jeremy Inman <jeremy.inman13@gmail.com>
2023-06-22 14:03:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 
 import streamlit
 
+#title
 streamlit.title("My Parents New Healthy Dinner")
+
+#header 1: breakfast
+streamlit.header("Breakfast Menu")
+
+#breakfast items
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free Range Egg")
+
