c0289a80f744b281b587f3cabf63a9c8d9c578bd
Jeremy Inman <Jeremy.Inman@nestle.com>
2023-06-26 11:18:56 -0500

    added furity vice section header and json from response

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#new section header
+streamlit.header("Fruityvice Fruit Advice!")
 
 #test input api 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
