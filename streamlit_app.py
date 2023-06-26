b9de98df285881adbd1fb991f80377d07d5889a0
Jeremy Inman <Jeremy.Inman@nestle.com>
2023-06-26 11:14:29 -0500

    added fruityvice api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 
 import streamlit
 import pandas
+import requests
+
+#test input api 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 #handle data steams here
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
