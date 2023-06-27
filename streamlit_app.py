18bff67b9a43a85cfdb44a69669908bbbf8ac883
Jeremy Inman <Jeremy.Inman@nestle.com>
2023-06-27 08:51:07 -0500

    added some cleanup items

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,10 +75,11 @@ def get_fruit_load_list():
 		return my_cur.fetchall()
 
 #add a button to make the call 
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('View Our Fruit List - Add Your Favorites!'):
 
 	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 	my_data_rows =get_fruit_load_list()
+	my_cnx.close()
 	streamlit.dataframe(my_data_rows)
 
 #function to insert fruit rows 
@@ -98,5 +99,6 @@ add_my_fruit =  streamlit.text_input('What fruit would you like to add?','jackfr
 if streamlit.button('Add a Fruit to the List'):
 	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 	added_fruit = insert_row_snowflake(add_my_fruit)
+	my_cnx.close()
 	streamlit.text(added_fruit)
 
