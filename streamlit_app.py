3cfb0afd5cc8f4c9d2f743d1297c5ba245f4f5fc
Jeremy Inman <Jeremy.Inman@nestle.com>
2023-06-27 08:16:09 -0500

    added a user input text box for them to enter what fruit to add.

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,3 +55,8 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("This fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#text box for user to input a new fruit they want	
+add_my_fruit =  streamlit.text_input('What fruit would you like to add?','jackfruit')
+
+#TODO: ADD LOGIC TO INSERT INPUTS
