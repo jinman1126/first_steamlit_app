919cb0487119870df80018ecff78dd28f7e48203
Jeremy Inman <Jeremy.Inman@nestle.com>
2023-06-27 08:19:22 -0500

    added error handling import and stopped streamlit at snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 #handle data steams here
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -48,6 +49,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #write formatted data to the stream
 streamlit.dataframe(fruityvice_normalized)
 
+streamlist.stop()
 #import data from snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
